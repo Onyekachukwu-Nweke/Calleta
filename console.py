8e6be5fd5be15afd5350850786187423e918fb2b
Onyekachukwu-Nweke <nwekeejioforscheller@gmail.com>
2023-05-16 00:19:10 +0100

    feat: Adding starting and quitting functionality to console

--- console.py
+++ console.py
@@ -2,10 +2,27 @@
 " Console application for Calleta"
 import cmd
 import sys
-from basemodel import BaseModel
+# from basemodel import BaseModel
+import shlex
 
 
 class Calleta(cmd.Cmd):
     """
-    
+    Core functionality of the Calleta console app
     """
+    prompt = '(cale) '
+
+    def do_EOF(self, arg):
+        """Exits console"""
+        return True
+
+    def emptyline(self):
+        """ overwriting the emptyline method """
+        return False
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+if __name__ == '__main__':
+    Calleta().cmdloop()
